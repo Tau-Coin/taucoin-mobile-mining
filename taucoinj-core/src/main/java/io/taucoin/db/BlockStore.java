a8ec5b61d704756bc692694591340dd51454fd00
shunli.wang <shunli.wang@dwfund.net>
2019-01-16 11:09:11 +0800

    alternative blockstore to map block with it's index

---
+++ taucoinj-core/src/main/java/io/taucoin/db/BlockStore.java
@@ -0,0 +1,50 @@
+package io.taucoin.db;
+
+import io.taucoin.core.Block;
+import io.taucoin.core.BlockHeader;
+import org.hibernate.SessionFactory;
+
+import java.math.BigInteger;
+import java.util.List;
+
+/**
+ * @author Roman Mandeleil
+ * @since 08.01.2015
+ * @author taucoin core
+ * @since 01.16.2019
+ */
+public interface BlockStore {
+
+    byte[] getBlockHashByNumber(long blockNumber);
+
+    Block getChainBlockByNumber(long blockNumber);
+
+    Block getBlockByHash(byte[] hash);
+    boolean isBlockExist(byte[] hash);
+
+    List<byte[]> getListHashesEndWith(byte[] hash, long qty);
+
+    List<BlockHeader> getListHeadersEndWith(byte[] hash, long qty);
+
+    List<Block> getListBlocksEndWith(byte[] hash, long qty);
+
+    void saveBlock(Block block, BigInteger cummDifficulty, boolean mainChain);
+
+    BigInteger getTotalDifficultyForHash(byte[] hash);
+
+    BigInteger getTotalDifficulty();
+
+    Block getBestBlock();
+
+    long getMaxNumber();
+
+
+    void flush();
+
+    void reBranch(Block forkBlock);
+
+    void load();
+    void setSessionFactory(SessionFactory sessionFactory);
+
+
+}
