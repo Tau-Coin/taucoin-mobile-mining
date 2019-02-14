b02352043bca042714f15e03100ee0cfe4b268b2
shunli.wang <shunli.wang@dwfund.net>
2019-02-14 11:45:54 +0800

    import privkey supporting format base58 or hex string when init app

--- taucoinj-core-android/src/main/java/io/taucoin/android/Taucoin.java
+++ taucoinj-core-android/src/main/java/io/taucoin/android/Taucoin.java
@@ -2,11 +2,9 @@ package io.taucoin.android;
 
 
 import io.taucoin.android.manager.BlockLoader;
-import io.taucoin.core.Block;
-import io.taucoin.core.Genesis;
-import io.taucoin.core.PendingState;
-import io.taucoin.core.Transaction;
-import io.taucoin.core.Wallet;
+import io.taucoin.config.MainNetParams;
+import io.taucoin.core.*;
+import io.taucoin.crypto.ECKey;
 import io.taucoin.crypto.HashUtil;
 import io.taucoin.forge.BlockForger;
 import io.taucoin.manager.AdminInfo;
@@ -15,9 +13,12 @@ import io.taucoin.net.client.PeerClient;
 import io.taucoin.net.rlpx.discover.UDPListener;
 import io.taucoin.net.server.ChannelManager;
 import io.taucoin.net.server.PeerServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spongycastle.util.encoders.Hex;
 
-import java.util.List;
+import java.math.BigInteger;
+import java.util.*;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
@@ -27,7 +28,7 @@ import static io.taucoin.config.SystemProperties.CONFIG;
 
 @Singleton
 public class Taucoin extends io.taucoin.facade.TaucoinImpl {
-
+    private static final Logger log = LoggerFactory.getLogger("tauAndroid");
     @Inject
     public Taucoin(WorldManager worldManager, AdminInfo adminInfo,
                     ChannelManager channelManager, io.taucoin.manager.BlockLoader blockLoader, PendingState pendingState,
@@ -39,14 +40,31 @@ public class Taucoin extends io.taucoin.facade.TaucoinImpl {
     }
 
     public void init(List<String> privateKeys) {
+        List<ECKey> ecprivateKey = new ArrayList<>();
+        for (String prikey: privateKeys) {
+            ECKey key;
+            //String prikey = privateKeys.get(0);
+            log.info("privkey is {}",prikey);
 
-        for (String privateKey: privateKeys) {
-            worldManager.getWallet().importKey(Hex.decode(privateKey));
+            if (prikey.length() == 51 || prikey.length() == 52) {
+                DumpedPrivateKey dumpedPrivateKey = DumpedPrivateKey.fromBase58(MainNetParams.get(),prikey);
+                key = dumpedPrivateKey.getKey();
+            } else {
+                BigInteger privKey = new BigInteger(Hex.decode(prikey));
+                key = ECKey.fromPrivate(privKey);
+            }
+            ecprivateKey.add(key);
+        }
+
+        for (ECKey key: ecprivateKey) {
+           //worldManager.getWallet().importKey(Hex.decode(privateKey));
+           worldManager.getWallet().importKey(key.getPrivKeyBytes());
         }
 
         // By default, import first privkey as block forger private key.
-        if (!privateKeys.isEmpty()) {
-            CONFIG.importForgerPrikey(Hex.decode(privateKeys.get(0)));
+        if (!ecprivateKey.isEmpty()) {
+            //CONFIG.importForgerPrikey(Hex.decode(privateKeys.get(0)));
+            CONFIG.importForgerPrikey(ecprivateKey.get(0).getPrivKeyBytes());
         }
 
         worldManager.init();
