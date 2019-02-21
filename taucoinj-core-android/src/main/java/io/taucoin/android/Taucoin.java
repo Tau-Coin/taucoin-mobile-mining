305a807c26c05c35c94a32c75128e1637b94acd6
shunli.wang <shunli.wang@dwfund.net>
2019-02-21 15:02:09 +0800

    support WIF privatekey

--- taucoinj-core-android/src/main/java/io/taucoin/android/Taucoin.java
+++ taucoinj-core-android/src/main/java/io/taucoin/android/Taucoin.java
@@ -50,7 +50,7 @@ public class Taucoin extends io.taucoin.facade.TaucoinImpl {
                 DumpedPrivateKey dumpedPrivateKey = DumpedPrivateKey.fromBase58(MainNetParams.get(),prikey);
                 key = dumpedPrivateKey.getKey();
             } else {
-                BigInteger privKey = new BigInteger(Hex.decode(prikey));
+                BigInteger privKey = new BigInteger(prikey,16);
                 key = ECKey.fromPrivate(privKey);
             }
             ecprivateKey.add(key);
