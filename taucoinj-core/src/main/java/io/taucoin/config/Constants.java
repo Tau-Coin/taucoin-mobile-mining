fc21e5eb0aa8d28bb50ec00fb824ac2af3be4eb6
cuitaixiang <754060645@qq.com>
2019-02-21 11:49:54 +0800

    merge constants

--- taucoinj-core/src/main/java/io/taucoin/config/Constants.java
+++ taucoinj-core/src/main/java/io/taucoin/config/Constants.java
@@ -5,21 +5,22 @@ import java.math.BigInteger;
 import static io.taucoin.config.SystemProperties.CONFIG;
 
 public class Constants {
-    
+
+    //max block tx number
+    public static final int MAX_BLOCKTXSIZE = 50;
+    //transaction expiration height
+    public static final int TX_EXPIRATIONHEIGHT = 144;
+    //forge block time interval
+    public static final int BLOCKTIMEINTERVAL = 300;
+    //block time drift
+    public static final int MAX_TIMEDRIFT = 60; // allow up to 60 s clock difference
+
+    public static final String GENESIS_BLOCK_HASH = "0e19e167b7160f7ce173dbe8dbf50d2266365907";
+
     public static BigInteger MINIMUM_DIFFICULTY = BigInteger.valueOf(131072);
     public static BigInteger DIFFICULTY_BOUND_DIVISOR = BigInteger.valueOf(2048);
     public static int EXP_DIFFICULTY_PERIOD = 100000;
 
-    public static int UNCLE_GENERATION_LIMIT = 7;
-    public static int UNCLE_LIST_LIMIT = 2;
-
-    public static int BEST_NUMBER_DIFF_LIMIT = 100;
-
     public static final BigInteger SECP256K1N = new BigInteger("fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141", 16);
     public static final BigInteger SECP256K1N_HALF = SECP256K1N.divide(BigInteger.valueOf(2));
-    public static long HOMESTEAD_FORK_BLKNUM = 10_000_000;
-
-    public static int getDURATION_LIMIT() {
-        return CONFIG.isFrontier() ? 13 : 8;
-    }
 }
