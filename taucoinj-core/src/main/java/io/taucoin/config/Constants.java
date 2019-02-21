6e09ce7c21ae354e24794181c668cc268ce60f84
cuitaixiang <754060645@qq.com>
2019-02-21 11:23:39 +0800

    remove unused constants

--- taucoinj-core/src/main/java/io/taucoin/config/Constants.java
+++ taucoinj-core/src/main/java/io/taucoin/config/Constants.java
@@ -5,13 +5,7 @@ import java.math.BigInteger;
 import static io.taucoin.config.SystemProperties.CONFIG;
 
 public class Constants {
-
-    public static int GENESIS_DIFFICULTY = 131072;
-    public static int MAXIMUM_EXTRA_DATA_SIZE = 32;
-    public static int EPOCH_DURATION = 30_000;
-    public static int GENESIS_GAS_LIMIT = 3_141_592;
-    public static int MIN_GAS_LIMIT = 125000;
-    public static int GAS_LIMIT_BOUND_DIVISOR = 1024;
+    
     public static BigInteger MINIMUM_DIFFICULTY = BigInteger.valueOf(131072);
     public static BigInteger DIFFICULTY_BOUND_DIVISOR = BigInteger.valueOf(2048);
     public static int EXP_DIFFICULTY_PERIOD = 100000;
