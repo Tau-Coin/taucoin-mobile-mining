4b49f45acce3928fe5dff0bdcd8e4ded9d1bc2a2
shunli.wang <shunli.wang@dwfund.net>
2019-03-13 09:18:50 +0800

    process transaction although tag is false

--- taucoinj-core/src/main/java/io/taucoin/net/tau/handler/TauHandler.java
+++ taucoinj-core/src/main/java/io/taucoin/net/tau/handler/TauHandler.java
@@ -262,9 +262,9 @@ public abstract class TauHandler extends SimpleChannelInboundHandler<TauMessage>
     }
 
     private void processTransactions(TransactionsMessage msg) {
-        if(!processTransactions) {
-            return;
-        }
+//        if(!processTransactions) {
+//            return;
+//        }
 
         List<Transaction> txList = msg.getTransactions();
         Set<Transaction>  txSet = new HashSet<Transaction>();
