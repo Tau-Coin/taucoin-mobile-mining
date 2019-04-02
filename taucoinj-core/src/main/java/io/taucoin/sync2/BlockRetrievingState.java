c980b78739d6e470a6592e631834b8188be02191
shunli.wang <shunli.wang@dwfund.net>
2019-04-02 14:28:04 +0800

    implementation doMaintain method to 4 sync states trigger another or keep current

--- taucoinj-core/src/main/java/io/taucoin/sync2/BlockRetrievingState.java
+++ taucoinj-core/src/main/java/io/taucoin/sync2/BlockRetrievingState.java
@@ -1,12 +1,11 @@
 package io.taucoin.sync2;
 
-import static io.taucoin.net.tau.TauVersion.V61;
-import static io.taucoin.net.tau.TauVersion.V62;
 import static io.taucoin.sync2.SyncStateEnum.*;
 
 /**
  * @author Mikhail Kalinin
  * @since 13.08.2015
+ * @since 01.4.2019 by taucoin core
  */
 public class BlockRetrievingState extends AbstractSyncState {
 
@@ -18,7 +17,8 @@ public class BlockRetrievingState extends AbstractSyncState {
     public void doOnTransition() {
 
         super.doOnTransition();
-
+        //all peers change state to block retrieving.
+        //means that current time these peer are proper to do retrieving block work.
         syncManager.requestManager.changeSyncState(BLOCK_RETRIEVING);
     }
 
@@ -27,18 +27,22 @@ public class BlockRetrievingState extends AbstractSyncState {
 
         super.doMaintain();
 
+        //block queue is full or exceed maximum.
+        //if retrieving block further will lead to memory disaster.
         if (!syncManager.queue.isMoreBlocksNeeded()) {
             syncManager.changeState(IDLE);
             return;
         }
-        
-        if ((syncManager.queue.isHashesEmpty()  || !syncManager.requestManager.hasCompatible(V61)) &&
-            (syncManager.queue.isHeadersEmpty() || !syncManager.requestManager.hasCompatible(V62))) {
+
+        //hash queue is empty.
+        //there are no blocks corresponding to hashes for retrieving.
+        if (syncManager.queue.isHashesEmpty()) {
 
             syncManager.changeState(IDLE);
             return;
         }
-
+        //app node only has a peer,so this peer will be very busy.
+        //make use of every single idle free peer node.
         syncManager.requestManager.changeStateForIdles(BLOCK_RETRIEVING);
     }
 }
