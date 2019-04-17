586f7ecb708be01ffb4f9a8e172ce2768026e57c
shengxianshui <shengxianshui@iMorpheus.io>
2019-04-17 13:50:23 +0800

    Config bootup node address

--- taucoinj-core/src/main/java/io/taucoin/http/discovery/PeersManager.java
+++ taucoinj-core/src/main/java/io/taucoin/http/discovery/PeersManager.java
@@ -1,16 +1,19 @@
 package io.taucoin.http.discovery;
 
+import io.taucoin.crypto.ECKey;
 import io.taucoin.net.rlpx.Node;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.URI;
 import java.util.*;
 import java.util.concurrent.*;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import static io.taucoin.config.SystemProperties.CONFIG;
+import static io.taucoin.crypto.HashUtil.sha3;
 
 /**
  * @author Taucoin Core Developers
@@ -32,7 +35,9 @@ public class PeersManager {
         List<String> bootPeers = CONFIG.httpDiscoveryActivePeers();
 
         for (String boot: bootPeers) {
-            Node n = Node.instanceOf(boot);
+            URI uri = URI.create(boot);
+            final ECKey generatedNodeKey = ECKey.fromPrivate(sha3(boot.getBytes()));
+            Node n = new Node(generatedNodeKey.getNodeId(), uri.getHost(), uri.getPort());
             addActivePeer(n);
         }
         Collections.shuffle(activePeers);
