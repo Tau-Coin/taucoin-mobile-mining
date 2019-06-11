ddabe6db9840c50d76140cd68977f9272a1b21e1
shengxianshui <shengxianshui@iMorpheus.io>
2019-06-11 14:32:15 +0800

    Add log to print traffic statistics

--- taucoinj-core-android/src/main/java/io/taucoin/android/service/events/NetworkTrafficData.java
+++ taucoinj-core-android/src/main/java/io/taucoin/android/service/events/NetworkTrafficData.java
@@ -3,7 +3,23 @@ package io.taucoin.android.service.events;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Calendar;
+import java.util.TimeZone;
+
 public class NetworkTrafficData extends EventData {
+
+    private static final Logger logger = LoggerFactory.getLogger("TaucoinRemoteService");
+
+    private static long sAdjust = 0;
+    private static int sDay = -1;
+    private static long sDayTrafficStatistics = 0;
+    private static Calendar sCalendar = Calendar.getInstance(TimeZone.getDefault());
+    static {
+        sDay = sCalendar.get(Calendar.DAY_OF_MONTH);
+    }
 
     public int trafficSize;
 
@@ -11,6 +27,8 @@ public class NetworkTrafficData extends EventData {
 
         super();
         this.trafficSize = trafficSize;
+
+        printTrafficStatistics(trafficSize);
     }
 
     @Override
@@ -43,5 +61,25 @@ public class NetworkTrafficData extends EventData {
 
         super(in);
         trafficSize = in.readInt();
+    }
+
+    private static void printTrafficStatistics(int trafficSize) {
+        int currentYear = sCalendar.get(Calendar.YEAR);
+        int currentMonth = sCalendar.get(Calendar.MONTH) + 1;
+        int currentDay = sCalendar.get(Calendar.DAY_OF_MONTH);
+
+        if (currentDay == sDay) {
+            sAdjust += 1;
+            sDayTrafficStatistics += (long)trafficSize;
+        } else {
+            sAdjust = 0;
+            sDay = currentDay;
+            sDayTrafficStatistics = (long)trafficSize;
+        }
+
+        if (sAdjust % 10 == 0) {
+            logger.info("Taucoin traffic statistics: {} B {}/{}/{}",
+                    sDayTrafficStatistics, currentDay, currentMonth, currentYear);
+        }
     }
 }
